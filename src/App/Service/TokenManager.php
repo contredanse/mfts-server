54b4afff2a0aa702911ef2e2edfcda16dfc29015
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-20 15:05:08 +0100

    Token Manager Service

---
+++ src/App/Service/TokenManager.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
+use Lcobucci\JWT\Token;
+use Ramsey\Uuid\Uuid;
+
+class TokenManager {
+
+    private $signer;
+    private $issuer;
+    private $audience;
+    private $privateKey;
+
+    function __construct(string $privateKey) {
+
+        $this->signer = new Sha256();
+        $this->issuer = $_SERVER['SERVER_NAME'];
+        $this->audience = $_SERVER['SERVER_NAME'];
+        $this->privateKey = $privateKey;
+
+    }
+
+    function createNewToken(array $customClaims = [], int $expiration=3600, bool $autoSign=true): Token
+    {
+        $builder = (new Builder())
+            ->setIssuer($this->issuer) // Configures the issuer (iss claim)
+            ->setAudience($this->audience) // Configures the audience (aud claim)
+            ->setId(Uuid::uuid1(), true) // Configures the id (jti claim), replicating as a header item
+            ->setIssuedAt(time()) // Configures the time that the token was issue (iat claim)
+            ->setNotBefore(time() + 0) // Configures the time that the token can be used (nbf claim)
+            ->setExpiration(time() + $expiration); // Configures the expiration time of the token (exp claim)
+
+        foreach ($customClaims as $key => $value) {
+            $builder->set($key, $value);
+        }
+
+        if ($autoSign) {
+            return $this->signToken($builder);
+        }
+
+        return $token;
+    }
+
+    function signToken(Builder $builder): Token {
+        return $builder->sign($this->signer, $this->privateKey)
+            ->getToken(); // Retrieves the generated token
+    }
+}
---
+++ src/App/Service/TokenManagerFactory.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Exception\ConfigException;
+use Psr\Container\ContainerInterface;
+
+class TokenManagerFactory {
+
+    function __invoke(ContainerInterface $container): TokenManager {
+
+        $config = $container->get('config')['token_manager'] ?? null;
+        if ($config === null) {
+            throw new ConfigException("['token_manager'] config key is missing.");
+        }
+        if (strlen($config['privateKey'] ?? '') < 32) {
+            throw new ConfigException("['tokenManager']['privateKey'] config key is must be at least 32 chars long.");
+        }
+
+        return new TokenManager($config['privateKey']);
+    }
+}
