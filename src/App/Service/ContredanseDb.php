f29ff6bf79f371fc09895fb0fb6edf8bbb3e01ce
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-22 11:26:30 +0100

    Added new db service for contredanse

---
+++ src/App/Service/ContredanseDb.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Exception\ConnectionException;
+
+class ContredanseDb
+{
+
+    /**
+     * @var \PDO
+     */
+    private $pdo;
+
+    private $params;
+
+    public function __construct(array $params)
+    {
+    	$this->params = $params;
+    }
+
+	/**
+	 * @throws ConnectionException
+	 */
+    public function getPdoAdapter(): \PDO {
+    	if ($this->pdo === null) {
+    		$this->pdo = $this->createPdoConnection($this->params);
+		}
+		return $this->pdo;
+	}
+
+	/**
+	 * @param string[] $config
+	 *
+	 * @throws ConnectionException
+	 */
+	public function createPdoConnection(array $config): \PDO
+	{
+		$dsn = $config['dsn'];
+		/** @var string[] $options */
+		$options = $config['driver_options'] ?? null;
+
+		try {
+			$pdo = new \PDO($dsn, $config['username'], $config['password'], $options);
+			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+		} catch (\Throwable $e) {
+			throw new ConnectionException(sprintf(
+				'Database connection failure (%s)',
+				$e->getMessage()
+			));
+		}
+
+		return $pdo;
+	}
+
+}
+
---
+++ src/App/Service/ContredanseDbFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Exception\ConfigException;
+use App\Exception\ConnectionException;
+use Psr\Container\ContainerInterface;
+
+class ContredanseDbFactory
+{
+    public function __invoke(ContainerInterface $container): ContredanseDb
+    {
+		$config = $container->get('config')['contredanse'] ?? null;
+		if ($config === null) {
+			throw new ConfigException("['contredanse'] config key is missing.");
+		}
+		if (!is_array($config['db'] ?? false)) {
+			throw new ConfigException("['contredanse']['db'] config key is missing.");
+		}
+
+		return new ContredanseDb(
+			$config['db']
+		);
+    }
+
+}
