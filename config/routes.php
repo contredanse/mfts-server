9becc28120f1ac03f6714ad08b4222e4c70ca574
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-22 17:23:23 +0100

    Updated routes doc

--- config/routes.php
+++ config/routes.php
@@ -7,35 +7,18 @@ use Zend\Expressive\Application;
 use Zend\Expressive\Helper\BodyParams\BodyParamsMiddleware;
 use Zend\Expressive\MiddlewareFactory;
 
-/**
- * Setup routes with a single request method:
- *
- * $app->get('/', App\Handler\HomePageHandler::class, 'home');
- * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
- * $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
- * $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
- * $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
- *
- * Or with multiple request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
- *
- * Or handling all request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class)->setName('contact');
- *
- * or:
- *
- * $app->route(
- *     '/contact',
- *     App\Handler\ContactHandler::class,
- *     Zend\Expressive\Router\Route::HTTP_METHOD_ANY,
- *     'contact'
- * );
- */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
+
+	// The famous temporary home page :)
+
     $app->get('/', App\Handler\HomePageHandler::class, 'home');
+
+    // A ping monitoring addess
+
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+
+
+    // Routes for JWT auth and validation
 
     $app->route('/api/auth/{action:token|validate}',
         [BodyParamsMiddleware::class, App\Handler\ApiAuthTokenHandler::class],
@@ -43,16 +26,12 @@ return function (Application $app, MiddlewareFactory $factory, ContainerInterfac
         'api.auth.token'
     );
 
+    // To monitor link with contredanse database
+
 	$app->get('/api/contredanse_status', App\Handler\ApiContredanseStatusHandler::class, 'api.contredanse.status');
 
+	// The profile data (protected by AuthTokenMiddleware
 
-    // All '/api below will be authenticated with AuthTokenMiddleware
-/*
-    $app->pipe('/api/v1', [
-    	\App\Middleware\AuthTokenMiddleware::class,
-		BodyParamsMiddleware::class,
-	]);
-*/
     $app->get('/api/v1/profile',
 		[\App\Middleware\AuthTokenMiddleware::class, \App\Handler\ApiContredanseProfileHandler::class],
 		'api.v1.profile'
