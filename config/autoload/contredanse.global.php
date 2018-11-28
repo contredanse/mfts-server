ce8e8d15c199491eb73a416fea178807ebe544cf
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-11-28 20:19:42 +0100

    Added protocol compression

--- config/autoload/contredanse.global.php
+++ config/autoload/contredanse.global.php
@@ -10,7 +10,8 @@ return [
             'charset'   => 'utf8',
             'driver_options' => [
                 \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8",
-				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+				\PDO::MYSQL_ATTR_COMPRESS => true,
             ],
         ],
 
