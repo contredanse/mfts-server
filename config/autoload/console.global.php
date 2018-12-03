28c835283951e10d1cf39dfefe643d60bbfdfc10
Sébastien Vanvelthem <belgattitude@gmail.com>
2018-12-03 18:59:46 +0100

    registering dbdump commands

--- config/autoload/console.global.php
+++ config/autoload/console.global.php
@@ -7,13 +7,15 @@ return [
 
         'factories' => [
             App\Command\GenerateVideoCoversCommand::class => App\Command\GenerateVideoCoversCommandFactory::class,
-            App\Command\TranscodeVideosCommand::class => App\Command\TranscodeVideosCommandFactory::class
+            App\Command\TranscodeVideosCommand::class => App\Command\TranscodeVideosCommandFactory::class,
+			App\Command\DbDumpCommand::class => App\Command\DbDumpCommandFactory::class,
         ],
     ],
     'console' => [
         'commands' => [
             App\Command\GenerateVideoCoversCommand::class,
             App\Command\TranscodeVideosCommand::class,
+			App\Command\DbDumpCommand::class
         ],
     ],
 ];
